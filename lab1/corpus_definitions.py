80ba980c17a001b612c9fb7e2c62ee2d804b9733
piotroramus <piotroramus@mail.com>
2017-03-07 18:19:27 +0100

    Finished lang statistics

--- lab1/corpus_definitions.py
+++ lab1/corpus_definitions.py
@@ -7,90 +7,90 @@ _corpuses = [
         'filename': 'english_ex.txt',
         'lang': 'en',
     },
-    # {
-    #     'filename': 'finnish_short.txt',
-    #     'lang': 'fl',
-    # },
-    # {
-    #     'filename': 'english1.txt',
-    #     'lang': 'en',
-    # },
-    # {
-    #     'filename': 'english2.txt',
-    #     'lang': 'en',
-    # },
-    # {
-    #     'filename': 'english3.txt',
-    #     'lang': 'en',
-    # },
-    # {
-    #     'filename': 'english4.txt',
-    #     'lang': 'en',
-    #     'charset': 'ISO-8859-2',
-    # },
-    # {
-    #     'filename': 'finnish1.txt',
-    #     'lang': 'fl',
-    #     'charset': 'iso-8859-9',
-    # },
-    # {
-    #     'filename': 'finnish2.txt',
-    #     'lang': 'fl',
-    #     'charset': 'iso-8859-9',
-    # },
-    # {
-    #     'filename': 'german1.txt',
-    #     'lang': 'de',
-    #     'charset': 'iso-8859-1',
-    # },
-    # {
-    #     'filename': 'german2.txt',
-    #     'lang': 'de',
-    #     'charset': 'iso-8859-1',
-    # },
-    # {
-    #     'filename': 'german3.txt',
-    #     'lang': 'de',
-    #     'charset': 'iso-8859-1',
-    # },
-    # {
-    #     'filename': 'german4.txt',
-    #     'lang': 'de',
-    #     'charset': 'iso-8859-1',
-    # },
-    # {
-    #     'filename': 'italian1.txt',
-    #     'lang': 'it',
-    # },
-    # {
-    #     'filename': 'italian2.txt',
-    #     'lang': 'it',
-    # },
-    # {
-    #     'filename': 'polish1.txt',
-    #     'lang': 'pl',
-    #     'charset': 'iso-8859-2',
-    # },
-    # {
-    #     'filename': 'polish2.txt',
-    #     'lang': 'pl',
-    #     'charset': 'iso-8859-2',
-    # },
-    # {
-    #     'filename': 'polish3.txt',
-    #     'lang': 'pl',
-    #     'charset': 'iso-8859-2',
-    # },
-    # {
-    #     'filename': 'spanish1.txt',
-    #     'lang': 'es',
-    #     'charset': 'iso-8859-9',
-    # },
-    # {
-    #     'filename': 'spanish2.txt',
-    #     'lang': 'es',
-    #     'charset': 'iso-8859-9',
-    # },
+    {
+        'filename': 'finnish_short.txt',
+        'lang': 'fl',
+    },
+    {
+        'filename': 'english1.txt',
+        'lang': 'en',
+    },
+    {
+        'filename': 'english2.txt',
+        'lang': 'en',
+    },
+    {
+        'filename': 'english3.txt',
+        'lang': 'en',
+    },
+    {
+        'filename': 'english4.txt',
+        'lang': 'en',
+        'charset': 'ISO-8859-2',
+    },
+    {
+        'filename': 'finnish1.txt',
+        'lang': 'fl',
+        'charset': 'iso-8859-9',
+    },
+    {
+        'filename': 'finnish2.txt',
+        'lang': 'fl',
+        'charset': 'iso-8859-9',
+    },
+    {
+        'filename': 'german1.txt',
+        'lang': 'de',
+        'charset': 'iso-8859-1',
+    },
+    {
+        'filename': 'german2.txt',
+        'lang': 'de',
+        'charset': 'iso-8859-1',
+    },
+    {
+        'filename': 'german3.txt',
+        'lang': 'de',
+        'charset': 'iso-8859-1',
+    },
+    {
+        'filename': 'german4.txt',
+        'lang': 'de',
+        'charset': 'iso-8859-1',
+    },
+    {
+        'filename': 'italian1.txt',
+        'lang': 'it',
+    },
+    {
+        'filename': 'italian2.txt',
+        'lang': 'it',
+    },
+    {
+        'filename': 'polish1.txt',
+        'lang': 'pl',
+        'charset': 'iso-8859-2',
+    },
+    {
+        'filename': 'polish2.txt',
+        'lang': 'pl',
+        'charset': 'iso-8859-2',
+    },
+    {
+        'filename': 'polish3.txt',
+        'lang': 'pl',
+        'charset': 'iso-8859-2',
+    },
+    {
+        'filename': 'spanish1.txt',
+        'lang': 'es',
+        'charset': 'iso-8859-9',
+    },
+    {
+        'filename': 'spanish2.txt',
+        'lang': 'es',
+        'charset': 'iso-8859-9',
+    },
 ]
 
 
--- lab1/lang_ngrams.py
+++ lab1/lang_ngrams.py
@@ -1,4 +1,5 @@
 import codecs
+import glob
 import os
 import re
 import sys
@@ -6,7 +7,22 @@ import sys
 from collections import Counter
 from unidecode import unidecode
 
-from corpus_definitions import get_corpuses
+from corpus_definitions import get_corpuses, languages_set
+
+
+def build_lang_statistics(n, lang, corpus_results_dir, lang_results_dir):
+    # filenaming convention is prefix_lang_n.txt
+    pattern = "*_{}_{}.txt".format(lang, n)
+
+    result = Counter()
+    for fn in glob.glob(os.path.join(corpus_results_dir, pattern)):
+        with open(fn, 'r') as f:
+            for line in f:
+                ngram, count = line.split(' ')
+                result[ngram] += int(count)
+
+    output_file = os.path.join(lang_results_dir, "{}_{}.txt".format(lang, n))
+    save_results(output_file, result)
 
 
 def determine_ngrams(data, n):
@@ -30,19 +46,19 @@ def determine_ngrams(data, n):
 
 def generate_statistics(n, results_dir):
     for corpus in get_corpuses():
-        print "Processing corpus {}...".format(corpus['filename'])
+        print "Building {}-grams for {}...".format(n, corpus['filename'])
         with codecs.open(corpus['filename'], 'r', corpus.get('charset', 'utf-8')) as fn:
             stat = determine_ngrams(fn.read(), n)
-            print stat
             filename_without_extension = corpus['filename'].split('/')[-1].split('.')[0]
-            output_file = os.path.join(results_dir, "{}_{}.txt".format(filename_without_extension, n))
+            output_file = os.path.join(results_dir,
+                                       "{}_{}_{}.txt".format(filename_without_extension, corpus['lang'], n))
             save_results(output_file, stat)
 
 
-def save_results(filename, result):
+def save_results(filename, dictionary):
     with open(filename, 'w') as fn:
-        for ngram in result:
-            fn.write("{} {}\n".format(ngram, result[ngram]))
+        for key in dictionary:
+            fn.write("{} {}\n".format(key, dictionary[key]))
 
 
 if __name__ == '__main__':
@@ -52,6 +68,12 @@ if __name__ == '__main__':
 
     n = int(sys.argv[1])
 
-    results_dir = "results"
+    corpus_results_dir = os.path.join("results", "corpus")
+    lang_results_dir = os.path.join("results", "lang")
 
-    generate_statistics(n, results_dir)
+    generate_statistics(n, corpus_results_dir)
+
+    available_languages = languages_set()
+    for lang in available_languages:
+        print "Gathering statistics for {} language...".format(lang)
+        build_lang_statistics(n, lang, corpus_results_dir, lang_results_dir)
