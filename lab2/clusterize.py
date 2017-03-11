7faa9a8916a31ec218f66a983c704d90eda9f112
piotroramus <piotroramus@mail.com>
2017-03-11 19:30:13 +0100

    Implemented levenshtein metric

---
+++ lab2/clusterize.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+
+def levenshtein_distance(s1, s2):
+    n, m = len(s1), len(s2)
+    if n == 0:
+        return m
+    if m == 0:
+        return n
+    matrix = [[0 for _ in xrange(n+1)] for _ in xrange(m+1)]
+    for i in xrange(m+1):
+        matrix[i][0] = i
+    for i in xrange(n+1):
+        matrix[0][i] = i
+
+    for i, c1 in enumerate(s1):
+        for j, c2 in enumerate(s2):
+            cost = int(c1 != c2)
+            matrix[j+1][i+1] = min(matrix[j][i+1] + 1, matrix[j+1][i] + 1, matrix[j][i] + cost)
+    pprint(matrix)
+
+    return matrix[m][n]
