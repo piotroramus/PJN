804204bb719a0344ff6fbcf41573f15e246d3852
piotroramus <piotroramus@mail.com>
2017-05-29 20:20:47 +0200

    Finding prepositions followed by nouns

--- lab9/PlpWrapper.py
+++ lab9/PlpWrapper.py
@@ -15,11 +15,18 @@ class PlpWrapper(object):
                 nouns.append(pos)
         return nouns
 
+    def get_noun_ids(self, word):
+        return [clp_id for clp_id in self.plp.rec(word) if self.pos(clp_id) == PartOfSpeech.RZECZOWNIK]
+
     def get_preposition(self, word):
         for pos in self.part_of_speech(word):
             if pos == PartOfSpeech.PRZYIMEK:
                 return pos
         return None
+
+    def parts_of_grammar(self, word, word_id):
+        vec = self.plp.vec(word_id, word)
+        return list(set([PartOfSpeech((i - 1) % 7 + 1) for i in vec]))
 
     def part_of_speech(self, word):
         parts = list()
@@ -44,3 +51,51 @@ class PartOfSpeech(Enum):
     SPOJNIK = 9
     NIEODMIENNY = 10
     SKROT = 11
+
+    str_map = {
+        0: "???",
+        1: "RZECZOWNIK",
+        2: "CZASOWNIK",
+        3: "PRZYMIOTNIK",
+        4: "LICZEBNIK",
+        5: "ZAIMEK",
+        6: "PRZYSLOWEK",
+        7: "WYKRZYNIK",
+        8: "PRZYIMEK",
+        9: "SPOJNIK",
+        10: "NIEODMIENNY",
+        11: "SKROT"
+    }
+
+    def __str__(self):
+        return self.str_map[self.value]
+
+
+class GrammarCase(Enum):
+    MIANOWNIK = 1
+    DOPELNIACZ = 2
+    CELOWNIK = 3
+    BIERNIK = 4
+    NARZEDNIK = 5
+    MIEJSCOWNIK = 6
+    WOLACZ = 7
+    MIANOWNIK_MN = 8
+    DOPELNIACZ_MN = 9
+    CELOWNIK_MN = 10
+    BIERNIK_MN = 11
+    NARZEDNIK_MN = 12
+    MIEJSCOWNIK_MN = 13
+    WOLACZ_MN = 14
+
+    str_map = {
+        1: "MIANOWNIK",
+        2: "DOPELNIACZ",
+        3: "CELOWNIK",
+        4: "BIERNIK",
+        5: "NARZEDNIK",
+        6: "MIEJSCOWNIK",
+        7: "WOLACZ"
+    }
+
+    def __str__(self):
+        return self.str_map[(self.value - 1) % 7 + 1]
--- lab9/preprocess.py
+++ lab9/preprocess.py
@@ -2,7 +2,13 @@
 
 import io
 import re
-from collections import defaultdict
+import string
+
+from collections import defaultdict, Counter
+
+from PlpWrapper import PlpWrapper, GrammarCase
+
+plp = PlpWrapper()
 
 
 def split_to_sentences(input_file):
@@ -12,13 +18,44 @@ def split_to_sentences(input_file):
     sentence_pattern = r'\.\s+(?=[A-ZĆŁŚŻŹ])'
     sentence_split = re.compile(sentence_pattern)
 
-    sentences = defaultdict(list)
-    text_num = 0
+    remove_punctuation_pattern = re.compile('[%s]' % re.escape(string.punctuation))
+
+    sentences = list()
     with io.open(input_file, 'r', encoding='utf-8') as f:
         documents = re.split(new_doc_split, f.read())[1:]
         for doc in documents:
-            text_num += 1
             for sentence in re.split(sentence_split, doc):
-                sentences[text_num].append(sentence)
+                processed = remove_punctuation_pattern.sub(' ', sentence.strip()).lower()
+                sentences.append(processed)
 
     return sentences
+
+
+def prepositions(sentences):
+    result = defaultdict(set)
+
+    for sentence in sentences:
+        words = sentence.strip().split(' ')
+
+        word_index = 0
+        words_len = len(words)
+        while word_index < words_len:
+            word = words[word_index]
+            prep = plp.get_preposition(word)
+            if not prep:
+                word_index += 1
+                continue
+
+            # look until noun is encountered or sentence is over
+            next_word_index = word_index + 1
+            loop = True
+            while loop and next_word_index < words_len:
+                next_word = words[next_word_index]
+                next_word_index += 1
+                next_word_nouns = plp.get_noun_ids(next_word)
+                for noun in next_word_nouns:
+                    loop = False
+                    result[word].add((next_word, noun))
+            word_index += 1
+
+    return result
